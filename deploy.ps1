177b73f40a34042efa387afad7f4388bb5d6cabb
Ned Bellavance <ned.bellavance@gmail.com>
2019-10-08 20:40:17 -0400

    deploy script

---
+++ deploy.ps1
@@ -0,0 +1,27 @@
+#Log into Azure
+Add-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "PS" | Select-AzSubscription
+
+$ResourceGroupName = "tf-test1"
+$Location = "eastus"
+
+$TemplateParameters = @{
+    adminUsername = "tfadmin"
+    adminPasswordOrKey = 'n6Uz^)N.d!j+uE'
+}
+
+$DeploymentParameters = @{
+    Name = "tf-deploy"
+    ResourceGroupName = $ResourceGroupName
+    TemplateFile = ".\azuredeploy.json"
+    TemplateParameterObject = $TemplateParameters
+    Mode = "Incremental"
+}
+
+#Create the resource group for the VM
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+#Deploy the #Windows VM template
+New-AzResourceGroupDeployment @DeploymentParameters
